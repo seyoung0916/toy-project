17541392a3a758c9accd44fc3dbbbd422602167c
seyoung <seyoung0916@naver.com>
2021-07-18 12:52:08 +0900

    http 통신 test

---
+++ src/main/java/com/toy/blog/test/HttpControllerTest.java
@@ -0,0 +1,29 @@
+package com.toy.blog.test;
+
+import org.springframework.web.bind.annotation.*;
+
+// 사용자 요청 -> 응답 (data)
+@RestController
+public class HttpControllerTest {
+
+    @GetMapping("/http/get")
+    public String getTest(Member m) {
+        return "get 요청 " + m.getId() + ", " + m.getUsername();x`
+    }
+
+    // 인터넷 브라우저 요청은 get만 가능
+    @PostMapping("/http/post")
+    public String postTest() {
+        return "post 요청";
+    }
+
+    @PutMapping("/http/put")
+    public String putTest() {
+        return "put 요청";
+    }
+
+    @DeleteMapping("/http/delete")
+    public String deleteTest() {
+        return "delete 요청";
+    }
+}
